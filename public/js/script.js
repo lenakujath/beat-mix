6f7dddd25dba6581096eebd97df1273b536b8de6
Lena Kujath <l.kujath@gmx.de>
2020-09-27 15:49:18 +0200

    experimented with toggle function

--- public/js/script.js
+++ public/js/script.js
@@ -6,15 +6,26 @@ let rideCymbals = [false, false,false, false, false, false, false, false, false,
 
 
 const toggleDrums = (arr, index) => {
+
+  let toggledArray = [...arr];
   let currentId = arr[index];
   let newId = !currentId;   
+
+  toggledArray[index] = newId;
+  arr = toggledArray;
   
-  return newId;
+  return arr;
 }
 
-console.log(toggleDrums(kicks, 1))
+  console.log(toggleDrums(kicks, 18))
+ console.log(kicks)
 
 
-  
+
+
+
+
+
+
 
   
