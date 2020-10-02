9e5f79af8c549c656e071a863f3aab891bb9cfba
Lena Kujath <l.kujath@gmx.de>
2020-10-02 20:39:57 +0200

    added clear function

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,31 @@
 // Drum Arrays
-let kicks = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, ];
+let kicks = [true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, ];
 let snares = [false, false,false, false, false, false, false, false, false, false, false, false, false, false, false, false, ];
 let hiHats = [false, false,false, false, false, false, false, false, false, false, false, false, false, false, false, false, ];
 let rideCymbals = [false, false,false, false, false, false, false, false, false, false, false, false, false, false, false, false, ];
 
 
 const toggleDrums = (arr, index) => {
-
- 
   let currentId = arr[index];
   let newId = !currentId;   
   return arr[index] = newId;
-}
+};
 
-  toggleDrums(kicks, 0);
-  console.log(kicks)
-//  console.log(kicks)
+const clear = (arr) => {
+ for (let i = 0; i < arr.length; i++) {
+   if (arr[i] === true){
+     arr[i] = false;
+   }
+ }
+ return arr;
+}
+clear(kicks);
+console.log(kicks)
+
+
+
+
+
 
 
   
